22ad2b8729868bf7e171b07db3ecaa02e0ab9b3f
Blake Bishop <b.bishop3709@student.nu.edu>
2023-05-18 13:56:33 -0700

    Local: updated requirements and made some config changes to views.py

--- project/core/views.py
+++ project/core/views.py
@@ -2,12 +2,15 @@
 from flask import render_template, Blueprint, request, jsonify, send_file
 from sqlalchemy.orm import sessionmaker
 from project.config import engine
-import os
+import os, sys, logging, datetime
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 core = Blueprint('core', __name__)
+
+logging.basicConfig(format='%(levelness)s:%(pastime)s:  %(message)s',datefmt="%Y-%m-%d %H:%M", level=logging.INFO)
+TODAY = datetime.datetime.now()
 
 
 # ---------- ROUTE: INDEX ----------
